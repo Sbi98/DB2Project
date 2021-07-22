84822e2d7e0167e5ec5f4bf22429703417f92125
Sbi98 <ricciovi@hotmail.it>
2021-07-22 11:42:20 +0200

    Nuovo filtro, presentazione vince finita. Commit PRE cambi alle offensive word

--- J2EEProject/webModule/src/main/java/db2project/utils/LoginFilter.java
+++ J2EEProject/webModule/src/main/java/db2project/utils/LoginFilter.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 public class LoginFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		//System.out.print("Login checker filter executing ...\n");
 		//TODO così un admin può accedere alle pagine di un utente standard e fare review. Forse serve un altro filtro
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
